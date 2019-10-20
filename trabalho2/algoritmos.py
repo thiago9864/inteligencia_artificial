deb71d0215acceaf68a8dc0826241bcd91d5f264
Victor Aquiles <victoraqalencar@gmail.com>
2019-10-20 18:12:46 -0300

    Voltando para a ideia de caminho mais curto entre cidades

--- trabalho2/algoritmos.py
+++ trabalho2/algoritmos.py
@@ -1,4 +1,4 @@
-def busca_ordenada(grafo, origem):
+def busca_ordenada(grafo, origem, destino):
 
     # durante a execução cada nó terá o custo do caminho e a lista de nós do caminho
     atual = {'no': origem, 'custo': 0, 'caminho': [origem]}
@@ -6,23 +6,24 @@ def busca_ordenada(grafo, origem):
     fechados = []
 
     # Realizamos as iterações enquanto o caminho não passar por todos os nós e 
-    # voltar a origem
-    while len(atual['caminho']) < grafo.number_of_nodes() + 1:
+    # não encontrar o destino
+    while len(atual['caminho']) < grafo.number_of_nodes() and atual['no'] != destino:
         
         # adicionando novos nós a lista de abertos
         for adj in grafo[atual['no']]:
             
             # garante a não visitação de nós repetidos
-            # mas permite repetir o nó de origem se ele for na ultima posição
-            if adj not in atual['caminho'] or (adj == origem and len(atual['caminho']) == grafo.number_of_nodes()):
+            if adj not in atual['caminho']:
                     
                 novo_custo = grafo[atual['no']][adj]['weight'] + atual['custo']
                 novo_caminho = atual['caminho'] + [adj]
 
                 abertos.append({'no': adj, 'custo': novo_custo, 'caminho': novo_caminho})
-            
+                
         abertos.sort(key=lambda x: x['custo']) # ordenando lista de abertos pelo custo do caminho
-        fechados.append(atual) # adiciona no já visitado a lista de fechados
-        atual = abertos.pop(0) # visitando o nó de caminho mais curto
+        fechados.append(atual)   # adiciona no já visitado a lista de fechados 
+        proximo = abertos.pop(0) # visitando o nó de caminho mais curto
+        
+        atual = proximo
 
     return atual, abertos, fechados
