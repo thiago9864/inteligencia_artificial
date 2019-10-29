223e7512ada4de5b67c6aca1104e452153090884
Thiago Almeida <thiago9864@hotmail.com>
2019-10-29 18:14:49 -0300

    Melhorias na estrutura da base

--- trabalho2/trabalho.py
+++ trabalho2/trabalho.py
@@ -5,27 +5,50 @@ Created on Wed Oct  9 20:34:23 2019
 @author: Thiago
 """
 
-from grafo import Grafo
+from Grafo import Grafo
+from Arvore import Arvore
 
 #variaveis
 instancia_path = 'arad-bucarest.txt'
 
 #inicia o grafo principal
-grafo = Grafo(instancia_path)
-grafo.removeNo(1)      
-grafo.addAresta(1,3,10,0)
-grafo.setLabelVertice(1, 'Barra Mansa')
-grafo.salvarArquivoGraphViz('teste.gv')
+g = Grafo(instancia_path)  
+
+#verifica os nós
+print("\nVerifica os nós\n")
+ids = []
+for no in g.grafo:
+    print(no.id, type(no.id), no.label)
+    ids.append(no.id)
+    
+#verifica a função getNo
+print("\nTesta getNo()\n")
+for id in ids:
+    no = g.getNo(id)
+    print(no.id, no.label, no.getHeu())
+    
+
+#verifica a função getAresta
+print("\nTesta getAresta()\n")
+arestasOk = True
+for no in g.grafo:
+    for aresta in no.arestas:
+        if g.getAresta(no.id, aresta.destino) == False:
+            print("Aresta (%d, %d) não encontrada" % no.id, aresta.destino)
+            arestasOk=False
+        else:
+            print("Aresta (%d, %d)" % (no.id, aresta.destino))
+if arestasOk:
+    print("\nArestas Ok\n")
+
+
+
+arvore = Arvore()
 
 lista_abertos = []
+lista_abertos.append(arvore.gerarRaiz("Arad", 3, 96.5))
+lista_abertos.append(arvore.gerarFilho(lista_abertos[0], "Sibiu", 4, 99, 89.5))
+lista_abertos.append(arvore.gerarFilho(lista_abertos[0], "Zerind", 2, 140, 73.0))
 
-arvore = Grafo()
-lista_abertos.append(arvore.gerarRaiz(1, "Teste"))
-lista_abertos.append(arvore.gerarFilho(lista_abertos[0], 2, "F1"))
-lista_abertos.append(arvore.gerarFilho(lista_abertos[0], 9, "F2"))
-lista_abertos.append(arvore.gerarFilho(lista_abertos[0], 3, "F3"))
 
-lista_abertos.append(arvore.gerarFilho(lista_abertos[2], 3, "F3"))
-lista_abertos.append(arvore.gerarFilho(lista_abertos[2], 2, "F1"))
-
-arvore.salvarArquivoGraphViz('arvore.gv')
+arvore.salvarArquivoGraphViz("teste_arvore.gv")
